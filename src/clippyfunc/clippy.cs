d0d873eb4536c468c2cc4dc274af8c842897183e
Steve Griffith <stgriffi@microsoft.com>
2019-03-07 09:48:35 -0500

    The human torch was denied a bank loan

--- src/clippyfunc/clippy.cs
+++ src/clippyfunc/clippy.cs
@@ -55,7 +55,7 @@ namespace clippyfunc
             //string output = $" {top}\n< {text} >\n {bottom}\n \\\n  \\\n    __\n   /  \\\n   |  |\n   @  @\n   |  |\n   || |/\n   || ||\n   |\\_/|\n   \\___/ \n\n Sentiment Score: {sentimentDetail.documents[0].score}";
             string output = $" {top}\n< {text} >\n {bottom}\n \\\n  \\\n    __\n   /  \\\n   |  |\n   @  @\n   |  |\n   || |/\n   || ||\n   |\\_/|\n   \\___/ \n";
 
-            output += $"\n Host OS: {System.Runtime.InteropServices.RuntimeInformation.OSDescription}\n\n";
+            output += $"\n OS: {System.Runtime.InteropServices.RuntimeInformation.OSDescription}\n\n";
             
             return output != null
                 ? (ActionResult)new OkObjectResult(output)
