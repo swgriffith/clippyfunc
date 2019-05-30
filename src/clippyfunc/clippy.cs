4b8ae8accd97cabbdb89cc29d68dbb271f84eb01
Steve Griffith <steve.griffith@microsoft.com>
2019-05-30 13:33:49 -0400

    Added flex vol support and chart

--- src/clippyfunc/clippy.cs
+++ src/clippyfunc/clippy.cs
@@ -29,7 +29,7 @@ namespace clippyfunc
             var config = new ConfigurationBuilder()
                 .AddEnvironmentVariables()
                 .Build();
-                
+           
             string top = "";
             string bottom= "";
             for (int i = 0; i < text.Length+2; i++)
@@ -40,8 +40,16 @@ namespace clippyfunc
 
             string output = $" {top}\n< {text} >\n {bottom}\n \\\n  \\\n    __\n   /  \\\n   |  |\n   @  @\n   |  |\n   || |/\n   || ||\n   |\\_/|\n   \\___/ \n";
 
-            output += $"\n OS: {System.Runtime.InteropServices.RuntimeInformation.OSDescription}\n\n";
-            
+            output += $"\nOS: {System.Runtime.InteropServices.RuntimeInformation.OSDescription}\n\n";
+
+            //Check if KeyVault is enabled, pull the setting and write to output
+            if(Convert.ToBoolean(config["USE_KEYVAULT_FLEXVOL"]))
+            {
+                string secretPath = config["SECRET_PATH"];
+                var mySecretSetting = LoadKVSettings(secretPath);
+                output += $"\nSecret Value: {mySecretSetting}";
+            }
+
             return output != null
                 ? (ActionResult)new OkObjectResult(output)
                 : new BadRequestObjectResult("Please pass a message in the request body");
@@ -50,6 +58,27 @@ namespace clippyfunc
             catch(Exception ex)
             {
                 return new BadRequestObjectResult(ex.InnerException);
+            }
+        }
+
+        //If key vault flag is set this will load the file from the 
+        //mounted FlexVol on the pod
+        private static string LoadKVSettings(string secretPath)
+        {
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(secretPath))
+                {
+                // Read the stream to a string, and write the string to the console.
+                    String secretValue = sr.ReadToEnd();
+                    return secretValue;
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                throw e;
             }
         }
 
